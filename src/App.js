7608822c8ef30a829fe125902ed793a13f8630ee
DEEPAK KUMAR MANDAL <nomercyhunters@gmail.com>
2022-05-20 14:03:03 +0530

    added

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Todo from'./component/Todo.jsx'
+function App() {
+  return (
+    <div className="App">
+     <Todo/>
+    </div>
+  );
+}
+
+export default App;
